46df44ecca54aab7e67137994fa0df0c425ad580
almaz1 <khairullin7@yandex.ru>
2023-09-25 16:32:40 +0300

    изменение 2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main (int argс, char **argv)
 {
 printf("hello world!\n")\n;
 изменение 1 Добавил привет.
-изменение 2 almaz2 создан новая ветка !
-h
+изменение 3 almaz2 создан новая ветка !
+
 return 0;
 }
